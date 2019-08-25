e71a52b8459aad0ca18521b2500268bf4244bc4a
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2019-08-25 16:10:28 -0600

    Experiment with module 'yargs'
    The module 'yargs' is build from the ground up in node js. I like it. It
    is expressive and in active development. It has the capability to build
    an entire CLI modularly through its file system. I'm pretty sure I'm
    going to settle upon this one.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { argv, exit } from 'process';
+import * as yargs from 'yargs';
 
 export function main(args: string[]) {
   console.log('Hello, World! It\'s a change!');
@@ -8,6 +9,38 @@ export function main(args: string[]) {
 }
 
 if (require.main === module) {
-  const status = main(argv);
-  exit(status);
+  const argv = yargs
+    .usage('$0 [-h] [-v|-q] [--version] <command> ...')
+    .command('create <title>', 'Create a new journal entry', yargs => {
+      return yargs.positional('title', {
+        desc: 'Title of the entry to be made',
+        type: 'string'
+      });
+    })
+    .command('list [--oneline]', 'List all existing entries', yargs => {
+      return yargs.option('oneline', {
+        desc: 'Use one line per entry',
+        type: 'boolean'
+      });
+    })
+    .option('verbose', {
+      alias: 'v',
+      description: 'Execute with verbose output',
+      type: 'boolean'
+    })
+    .option('quiet', {
+      alias: 'q',
+      description: 'Execute while supressing output',
+      type: 'boolean'
+    })
+    .conflicts('verbose', 'quiet')
+    .help('help', 'Show this help message and exit')
+    .alias('help', 'h')
+    .describe('version', 'Show version number and exit')
+    .parse();
+
+    console.dir(argv)
+
+  // const status = main(argv);
+  // exit(status);
 }
