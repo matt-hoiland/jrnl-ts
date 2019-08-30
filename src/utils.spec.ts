7197c700b5f0f0f6ca6de341e63df6022ef9b1a0
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2019-08-29 22:48:31 -0600

    Brainstorm functionality for 'loadEntry'

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,4 +1,4 @@
-import { loadEntry, FileNotFoundError } from './utils';
+import { loadEntry } from './utils';
 import { MetaData } from './model/MetaData';
 import { Entry } from './model/Entry';
 import * as fs from 'fs';
@@ -9,7 +9,7 @@ const WELLFORMED_METADATA_NO_TAGS: MetaData = {
   title: "Dummy Title"
 };
 
-const TESTFILE = `
+const TESTFILE_GOOD = `
 # Dummy Title
 
 \`\`\`json
@@ -22,23 +22,48 @@ Body text
 describe("loadEntry", () => {
 
   beforeAll(() => {
-    fs.writeFileSync(WELLFORMED_METADATA_NO_TAGS.file_name, TESTFILE);
+    fs.writeFileSync(WELLFORMED_METADATA_NO_TAGS.file_name, TESTFILE_GOOD);
   });
 
   afterAll(() => {
     fs.unlinkSync(WELLFORMED_METADATA_NO_TAGS.file_name);
   });
 
-  it("should throw an error for a missing entry", () => {
-    const file = 'nonexistent';
-    expect(() => loadEntry(file)).toThrow(new FileNotFoundError(file));
+  it("rejects missing file", () => {
+    loadEntry('nonexisten', (error, entry) => {
+      expect(error).not.toBeNull();
+      expect(error!.code).toEqual('ENOENT');
+    });
   });
 
-  it("should successfully load good data", () => {
-    let entry: Entry | null = null;
-
-    entry = loadEntry(WELLFORMED_METADATA_NO_TAGS.file_name);
-
-    expect(entry).not.toBeNull();
-  });
+  // it("rejects non-text file", () => { fail(); });
+  // it("accepts markdown file extension", () => { fail(); });
+  //
+  // describe("entry format", () => {
+  //   it("rejects non-title text before MetaData", () => { fail(); });
+  //   it("accepts entry without a title line", () => { fail(); });
+  //   it("accepts entry with empty body", () => { fail(); });
+  // });
+  //
+  // describe("parsing MetaData", () => {
+  //   describe("json", () => {
+  //     it("rejects missing json", () => { fail(); });
+  //     it("rejects malformed json", () => { fail(); });
+  //     it("accepts wellformed json", () => { fail(); });
+  //     it("accepts when entry has multiple json blocks", () => { fail(); });
+  //   });
+  //
+  //   describe("properties", () => {
+  //     it("rejects extra properties", () => { fail(); });
+  //     it("rejects missing properties", () => { fail(); });
+  //     it("rejects duplicate tags", () => { fail(); });
+  //     it("accepts without 'tags' propertie", () => { fail(); });
+  //   });
+  //
+  //   describe("timestamp", () => {
+  //     it("rejects bad ISO time", () => { fail(); });
+  //     it("accepts UTC", () => { fail(); });
+  //     it("accepts timezone offsets", () => { fail(); });
+  //   });
+  // });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import { Entry } from './model/Entry';
 
 export class NotImplementedError extends Error {
@@ -5,12 +6,19 @@ export class NotImplementedError extends Error {
     super(`${name} has not yet been implemented`);
   }
 }
-export class FileNotFoundError extends Error {
-  constructor(filename: string) {
-    super(`File ${filename} not found`);
-  }
-}
 
-export function loadEntry(filename: string): Entry {
-  throw new NotImplementedError('loadEntry');
+
+/**
+ * loadEntry - asynchronously load Entry from file
+ *
+ * @param  {string} filename  path to file to be loaded
+ * @param  {(NodeJS.ErrnoException | null, Entry) => void} cb callback to be executed on success or fail
+ */
+export function loadEntry(
+  filename: string,
+  cb: (error: NodeJS.ErrnoException | null, entry?: Entry) => void
+): void {
+  fs.readFile(filename, (error, data) => {
+    cb(error);
+  });
 }
