77c1cb56a419643fd0509a1faa4ebec1741a8f37
Matt Hoiland <matthew.hoiland@vivint.com>
2019-09-04 17:57:28 -0600

    Break up loadEntry into unimplemented subfunctions

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { Entry } from './model/Entry';
+import { MetaData } from './model/MetaData';
 import { isBinaryFileSync } from 'isbinaryfile';
 import { isValidMetaData } from './model/MetaData.validator';
 
@@ -16,37 +17,59 @@ export class NotImplementedError extends Error {
  * @return  {Entry | null} cb callback to be executed on success or fail
  * @throws {NodeJS.ErrnoException} Error if file not found, incorrect file type, etc.
  */
-export function loadEntry(filename: string): Entry | null {
+export function loadEntry(filename: string): Entry {
   try {
-    const stats = fs.lstatSync(filename);
-    const data = fs.readFileSync(filename);
-    if (isBinaryFileSync(data, stats.size)) {
-      const err: NodeJS.ErrnoException = new Error(
-        `${filename} not a text file`
-      );
-      err.code = 'ERR_INVALID_FD_TYPE';
-      throw err;
+    const data = loadFile(filename);
+    if (!isValidEntryFormat(data)) {
+      throw new NotImplementedError('loadEntry#!isValidEntryFormat');
     }
-
-    const text = data.toString('utf-8');
-    const lines = text.split('\n').map(line => line.replace(/\s+$/, ''));
-    const blockOpen = lines.findIndex((line, i, a) => line === '```json');
-    const blockClose = lines.findIndex((line, i, a) => line === '```');
-    const mdText = lines.slice(blockOpen + 1, blockClose).join('\n');
-
-    const metadata = JSON.parse(mdText);
-
-    let bodyOpen = blockClose + 1;
-    while (bodyOpen < lines.length && lines[bodyOpen] === '') {
-      bodyOpen++;
-    }
-
-    if (isValidMetaData(metadata)) {
-      return new Entry(metadata, lines.splice(bodyOpen).join('\n'));
-    }
-
-    return null;
+    const metadata = extractMetaData(data);
+    const bodytext = extractBodyText(data);
+    return new Entry(metadata, bodytext);
   } catch (err) {
     throw err;
+  }
+}
+
+export function loadFile(filename: string): Buffer {
+  const stats = fs.lstatSync(filename);
+  const data = fs.readFileSync(filename);
+  if (isBinaryFileSync(data, stats.size)) {
+    const err: NodeJS.ErrnoException = new Error(
+      `${filename} not a text file`
+    );
+    err.code = 'ERR_INVALID_FD_TYPE';
+    throw err;
+  }
+  return data;
+}
+
+export function isValidEntryFormat(filename: string): boolean;
+export function isValidEntryFormat(data: Buffer): boolean;
+export function isValidEntryFormat(data: string | Buffer): boolean {
+  if (typeof(data) === 'string') {
+    return isValidEntryFormat(loadFile(data));
+  } else {
+    throw new NotImplementedError('isValidEntryFormat');
+  }
+}
+
+export function extractMetaData(filename: string): MetaData;
+export function extractMetaData(data: Buffer): MetaData;
+export function extractMetaData(data: string | Buffer): MetaData {
+  if (typeof(data) === 'string') {
+    return extractMetaData(loadFile(data));
+  } else {
+    throw new NotImplementedError('extractMetaData');
+  }
+}
+
+export function extractBodyText(filename: string): string;
+export function extractBodyText(data: Buffer): string;
+export function extractBodyText(data: string | Buffer): string {
+  if (typeof(data) === 'string') {
+    return extractBodyText(loadFile(data));
+  } else {
+    throw new NotImplementedError('extractBodyText');
   }
 }
