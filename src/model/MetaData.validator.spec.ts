adfa40a4c4f1c19780273c9391a76f2fceb8bb0e
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2019-09-05 18:57:01 -0600

    Implement tests for Metadata validator

--- src/model/MetaData.validator.spec.ts
+++ src/model/MetaData.validator.spec.ts
@@ -2,64 +2,197 @@ import { isValidMetaData } from './MetaData.validator';
 
 describe('properties', () => {
   it("accepts without 'tags' property", () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00Z',
+        filename: '',
+        title: '',
+      })
+    ).toBeTruthy();
   });
 
   it("accepts empty 'tags' array", () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00Z',
+        filename: '',
+        title: '',
+        tags: [],
+      })
+    ).toBeTruthy();
   });
 
   it('rejects extra properties', () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00Z',
+        filename: '',
+        title: '',
+        star: true,
+      })
+    ).toBeFalsy();
   });
 
   it('rejects missing properties', () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00Z',
+        title: '',
+      })
+    ).toBeFalsy();
+    expect(
+      isValidMetaData({
+        filename: '',
+        title: '',
+      })
+    ).toBeFalsy();
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00Z',
+        filename: '',
+      })
+    ).toBeFalsy();
   });
 
   it('rejects duplicate tags', () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00Z',
+        filename: '',
+        title: '',
+        tags: ['a', 'b', 'a'],
+      })
+    ).toBeFalsy();
   });
 
   it('rejects required properties on type violation', () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: 0,
+        filename: '',
+        title: '',
+      })
+    ).toBeFalsy();
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00Z',
+        filename: true,
+        title: '',
+      })
+    ).toBeFalsy();
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00Z',
+        filename: '',
+        title: {},
+      })
+    ).toBeFalsy();
   });
 
   it("rejects 'tags' property on type violation", () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00Z',
+        filename: '',
+        title: '',
+        tags: 'star',
+      })
+    ).toBeFalsy();
   });
 });
 
 describe('ISOTime', () => {
   it('accepts UTC', () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00Z',
+        filename: '',
+        title: '',
+      })
+    ).toBeTruthy();
   });
 
   it('accepts timezone offsets', () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00-06:00',
+        filename: '',
+        title: '',
+      })
+    ).toBeTruthy();
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00.0000-06:00',
+        filename: '',
+        title: '',
+      })
+    ).toBeTruthy();
   });
 
   it('accepts timestamps with seconds', () => {
-    fail('Test not yet written');
-  });
-
-  it('accepts timestamps without seconds', () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00Z',
+        filename: '',
+        title: '',
+      })
+    ).toBeTruthy();
   });
 
   it('accepts timestamps with factional seconds', () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00.0000Z',
+        filename: '',
+        title: '',
+      })
+    ).toBeTruthy();
+  });
+
+  it('rejects timestamps without seconds', () => {
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00Z',
+        filename: '',
+        title: '',
+      })
+    ).toBeFalsy();
   });
 
   it('rejects timestamps with fractional seconds and NO seconds', () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00.0000Z',
+        filename: '',
+        title: '',
+      })
+    ).toBeFalsy();
   });
 
   it('rejects timestamps without timezone indicators', () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '1970-01-01T00:00:00',
+        filename: '',
+        title: '',
+      })
+    ).toBeFalsy();
   });
 
   it('rejects bad ISO time', () => {
-    fail('Test not yet written');
+    expect(
+      isValidMetaData({
+        date: '70-01-01T00:00Z',
+        filename: '',
+        title: '',
+      })
+    ).toBeFalsy();
+    expect(
+      isValidMetaData({
+        date: '1970-01-01 00:00Z',
+        filename: '',
+        title: '',
+      })
+    ).toBeFalsy();
   });
 });
