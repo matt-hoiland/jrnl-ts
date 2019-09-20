b3d750aa0cd6a0c86cc44d75e80792fab9e9db99
Matt Hoiland <matthew.hoiland@vivint.com>
2019-09-20 14:29:43 -0600

    Build out the `create` command module

---
+++ src/cmds/create.spec.ts
@@ -0,0 +1,14 @@
+/**
+ * Unit tests for `./create.ts`
+ *
+ * @author Matt Hoiland
+ * @date 2019-09-20
+ * @linces GPL-3.0
+ */
+
+ /**
+  * `create.handler` tests
+  */
+describe('create.hanlder', () => {
+
+});
