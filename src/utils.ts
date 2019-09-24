a7ba95bdda5ea6738e966d86cdbd2478beba937d
Matt Hoiland <matthew.hoiland@vivint.com>
2019-09-24 17:11:22 -0600

    Design and document simplifier function

---
+++ src/utils.ts
@@ -0,0 +1,41 @@
+/**
+ * # Utility Functions
+ *
+ * @author Matt Hoiland
+ * @date 2019-09-24
+ * @license GPL-3.0
+ */
+
+/** Imports */
+import { NotImplementedError } from './errors';
+
+/**
+ * Takes any single line string and produces a valid title part for the
+ * `filename` property of [[MetaData]]. Performs the following actions:
+ *
+ * 1. Split string on ` ` characters
+ * 2. Lowercase each substring
+ * 3. Drop all non alphanumeric characters
+ * 4. Throw out empty strings
+ * 5. Join substrings with `_` to not exceed the given `maxLength` defaulting to
+ * 32
+ *
+ * Due to the naïve restriction for matching the regex
+ * `/[a-z0-9][a-z0-9_]+[a-z0-9]/`, the minimum string length is by consequence
+ * 3. However, this function doesn't care as this will probably be patched
+ * later to be more robust.
+ *
+ * @param title The single line string to be simplified
+ * @param maxLength the length of the output string. The returned string will
+ *   stop at the last word that does not exceed the max length. If assigned
+ *   `false` or a value less than 0, the maxLength requirement will be ignored.
+ * @returns a string matching `/[a-z0-9][a-z0-9_]+[a-z]/` whose length does not
+ *   exceed `maxLength` unless `maxLength === false || maxLength < 0` in which
+ *   case the string length will not be considered
+ */
+export function simplifyTitle(
+  title: string,
+  maxLength: number | false = 32
+): string {
+  throw new NotImplementedError('simplifyTitle');
+}
